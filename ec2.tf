ebfd5555c06044c9c6ae33e7bf03b52d43db0c9b
Mauricio Afiuni <mauafiuni@gmail.com>
2024-03-26 20:52:20 +0100

    add import_demo ec2

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,8 @@ resource "aws_instance" "acme_app" {
     Name = var.business_zone
   }
 }
+
+#resource "aws_instance" "import_demo" {
+#  ami             = "ami-0c101f26f147fa7fd"
+#  instance_type   = var.ec2_type
+#}
